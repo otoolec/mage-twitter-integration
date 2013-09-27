c2703daa5476972d197d0b6083de4320bf3fac05
Christopher O'Toole <otoolec@x.com>
2013-09-27 16:10:13 -0500

    Add support for tweeting new products.

---
+++ app/Product.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class Product
+{
+    private $_name;
+    private $_price;
+    private $_url;
+
+    public function __construct($data)
+    {
+        $this->_parseData($data);
+    }
+
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    public function getPrice()
+    {
+        return $this->_price;
+    }
+
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+
+    private function _parseData($data)
+    {
+        if (isset($data['product'])) {
+            if (isset($data['product']['name'])) {
+                $this->_name = $data['product']['name'];
+            }
+            if (isset($data['product']['price'])) {
+                $this->_price =  $data['product']['price'];
+            }
+            if (isset($data['product']['url'])) {
+                $this->_url = $data['product']['url'];
+            }
+        }
+    }
+}
--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 require '../vendor/themattharris/tmhoauth/tmhOAuth.php';
 require '../vendor/themattharris/tmhoauth/tmhUtilities.php';
 require '../app/Order.php';
+require '../app/Product.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Silex\Application;
@@ -22,22 +23,61 @@ $app->before(function (Request $request) {
 });
 
 $app->post('/endpoint', function (Application $app, Request $request) {
-    if ($request->headers->get('Magento-Topic') === 'order/created') {
-        $app->finish(function(Request $request){
-            $data = $request->request->all();
-            $order = new Order($data);
-            postOrderToTwitter($order);
-        });
+    switch ($request->headers->get('Magento-Topic')) {
+        case 'order/created':
+            $app->finish(function(Request $request){
+                $data = $request->request->all();
+                $order = new Order($data);
+                postOrderToTwitter($order);
+            });
+            break;
+        case 'product/created':
+            $app->finish(function(Request $request){
+                $data = $request->request->all();
+                $order = new Product($data);
+                postProductToTwitter($order);
+            });
+            break;
     }
     return '';
 });
 
 $app->run();
 
+/**
+ * Tell people about a new product
+ *
+ * @param Product $product
+ * @return mixed
+ */
+function postProductToTwitter(Product $product)
+{
+    $message = "We are now selling {$product->getName()} for just {$product->getPrice()}!  Check it out at {$product->getUrl()}";
+
+    return postMessageToTwitter($message);
+}
+
+/**
+ * Brag about a sale.
+ *
+ * @param Order $order
+ * @return mixed
+ */
 function postOrderToTwitter(Order $order)
 {
     $message = "({$order->getId()}) Just sold {$order->getAmount()} {$order->getCurrency()} worth of merchandise.";
 
+    return postMessageToTwitter($message);
+}
+
+/**
+ * Post a message to twitter and return the response.
+ *
+ * @param $message
+ * @return mixed
+ */
+function postMessageToTwitter($message)
+{
     $ymlParser = new Parser();
     $credentials = $ymlParser->parse(file_get_contents('../twitter_credentials.yml'));
     $tmhOAuth = new tmhOAuth($credentials);
@@ -48,7 +88,7 @@ function postOrderToTwitter(Order $order)
 
     $response = $tmhOAuth->response['response'];
     if ($code != 200) {
-        error_log("Twitter says: {$code}");
+        error_log("Twitter says: {$code} - {$response} - in response to us sending it: {$message}");
     }
     return json_decode($response);
 }
